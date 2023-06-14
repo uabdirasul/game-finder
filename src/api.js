25df8db0f5699f075425bce9d9d7abf115cd45ea
uabdirasulm <ubbiniyazov04@gmail.com>
2023-06-14 19:26:33 +0500

    export popular games

--- src/api.js
+++ src/api.js
@@ -27,4 +27,4 @@ const lastYearDateInFormat = `${currentYear - 1}-${currentMonth}-${currentDay}`;
 const nextYearDateInFormat = `${currentYear + 1}-${currentMonth}-${currentDay}`;
 
 // Popular Games
-const popular_games_url = `${base_url}games?key=${api_key}&dates=${lastYearDateInFormat},${currentYearDateInFormat}&ordering=-rating&page_size`;
+export const popular_games_url = `${base_url}games?key=${api_key}&dates=${lastYearDateInFormat},${currentYearDateInFormat}&ordering=-rating&page_size`;
